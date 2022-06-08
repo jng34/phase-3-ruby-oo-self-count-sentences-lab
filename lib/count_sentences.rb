9827b5f4b85542fed2244f3f73b6a37c72540874
JamesNg <njames15@gmail.com>
2022-06-08 11:12:26 -0400

    commit tree 4128f64bebf901b5d8558a68bbe81232ae85c42a

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/\b\D+[\.!?]\b/).count
   end
 end
+
+puts "hello.".sentence?
